765525cb295b62b6430b80cf98c7ac89311d1012
Xyand <xyand@xyand.com>
2019-09-19 11:49:05 +0300

    Fix tmux config

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd $(dirname $0)
 cp .vimrc ~/.vimrc
 [ -d ~/.vim/bundle/Vundle.vim ] || git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-cp tmux.conf ~/
+cp .tmux.conf ~/
 cp .gitconfig ~/
 cp .bash_aliases ~/
 cp .bash_profile ~/
